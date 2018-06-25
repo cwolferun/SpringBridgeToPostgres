7427c08e003241aee0eb725f877f0cfb08d80e0a
Victor Blacknall <vBeta@localhost.localdomain>
2018-06-25 10:33:52 -0400

    threw

--- src/main/java/com/DeviceStatusController.java
+++ src/main/java/com/DeviceStatusController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -19,21 +20,32 @@ public class DeviceStatusController {
     DeviceStatusController() {
     }
 
+    //returns all available information for every every device in storage
     @GetMapping("/getall")
     List<Device> getAll() {
         return statusDao.getStatusFromDb();
     }
 
+    //returns a history of most recent 15 timestamps and network speeds for an individual device
     @GetMapping("/gethistory/{ip}")
     List<Map<String, Object>> gethistory(@PathVariable String ip) {
-        return statusDao.getHistory(ip.replaceAll("[.]", ""));
+
+        List<Map<String, Object>> entireHistory = statusDao.getHistory(ip.replaceAll("[.]", ""));
+
+        int historyLength = entireHistory.size();
+
+        if (historyLength<16){
+            return entireHistory;
+        }
+
+        return entireHistory.subList(historyLength-15,historyLength);
 
     }
+
+    //returns a the last store speed for a single device
     @GetMapping("/getonespeed/{ip}")
     String getOneSpeed(@PathVariable String ip) {
-
         return statusDao.getOneSpeed(ip);
-
     }
 
 
